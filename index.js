0bbdfafda1c21f0cc3caf2e649ee7bd31dd05394
饶家俊 <471412450@qq.com>
2021-06-01 17:58:29 +0800

    添加 yjhl create 命令

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const fs = require('fs')
 const ejs = require('ejs')
 const downloadGitRepo = require('download-git-repo') // 不支持 Promise
+const { program } = require('commander');
 
 
 
@@ -31,7 +32,16 @@ console.log('cwdUrl', cwdUrl);
 //     })
 // })
 
-downloadGitRepo('github:wojiaofengzhongzhuifeng/bos-vanilla#main', cwdUrl, { clone: false }, function (err) {
-    console.log(err);
-    console.log(err ? 'Error' : 'Success')
-})
+// downloadGitRepo('github:wojiaofengzhongzhuifeng/bos-vanilla#main', cwdUrl, { clone: false }, function (err) {
+//     console.log(err);
+//     console.log(err ? 'Error' : 'Success')
+// })
+
+program
+  .command('create [destination]')
+  .description('创建一个模板代码')
+  .action((destination) => {
+      console.log(path.join(cwdUrl, destination));
+  });
+
+program.parse();
